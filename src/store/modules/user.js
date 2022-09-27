83decd742c66cf94cdbc0614a0fe514ff10524e0
lqmzp <2792643139@qq.com>
2022-09-27 10:43:38 +0800

    主页完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -4,7 +4,8 @@ export default {
   namespaced: true,
   state: {
     token: null,
-    userInfo: {}
+    userInfo: {},
+    hrsaasTime: 0
   },
   mutations: {
     settoken(state, token) {
@@ -15,8 +16,13 @@ export default {
     },
     REOMVE_USER_INFO(state) {
       state.userInfo = {}
+    },
+    REOMVE_TOKEN(state) {
+      state.token = null
+    },
+    SET_HRSAASTIME(state, hrsaasTime) {
+      state.hrsaasTime = hrsaasTime
     }
-
   },
   actions: { // 执行异步
     // 定义login action  也需要参数 调用action时 传递过来的参数
@@ -25,6 +31,7 @@ export default {
       const data = await login(data1)
       console.log(data)
       context.commit('settoken', data)
+      context.commit('SET_HRSAASTIME', new Date().getTime())
     },
     async getUserInfos({ commit }) { // 哪里想用 哪掉
       // 发送请求获取用户token
@@ -32,7 +39,12 @@ export default {
       const res1 = await getUserDetailById(res.userId)
       const result = { ...res, ...res1 }
       commit('SET_USER_INFO', result) // 将整个的个人信息设置到用户的vuex数据中
+
       // return result // 这里为什么要返回 为后面埋下伏笔
+    },
+    logout({ commit }) {
+      commit('REOMVE_USER_INFO')
+      commit('REOMVE_TOKEN')
     }
   }
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,13 +1,29 @@
+import router from '@/router'
 import store from '@/store'
 import axios from 'axios'
 import { Message } from 'element-ui'
-
+const TimeOut = 3600
+function IsCheckTimeOut() {
+  const currentTime = Date.now()
+  const TimeStamp = (currentTime - store.getters.hrsaasTime) / 1000
+  // 当store.getters.hrsaasTime为0的时候TimeStamp>0
+  console.log(TimeStamp)
+  console.log(store.getters.hrsaasTime)
+  // console.log(1)
+  return TimeStamp > TimeOut
+}
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
 service.interceptors.request.use(config => {
   if (store.getters.token) {
+    if (IsCheckTimeOut()) {
+      console.log(2)
+      store.dispatch('user/logout')
+      router.push('/login')
+      return Promise.reject(new Error('token超时'))
+    }
     config.headers.Authorization = `Bearer ${store.getters.token}`
   }
   return config
@@ -22,7 +38,13 @@ service.interceptors.response.use(response => {
   Message.error(message)
   return Promise.reject(new Error(message))
 }, error => {
-  Message.error(error.message)
+  console.log(error)
+  if (error.response && error.response.status === 401) {
+    store.dispatch('user/logout')
+    router.push('/login')
+  } else {
+    Message.error(error.message)
+  }
   return Promise.reject(error)
 })
 
